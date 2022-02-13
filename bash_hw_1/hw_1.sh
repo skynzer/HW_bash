693cb276fb4fe2365bae190b489818ddc6ef134c
skynzer <skynzer@yandex.ru>
2022-02-13 10:34:07 +0300

    First homework correction. Add one-string command to move/copy multiple files. Change while-loop in script to previous command.
    Signed-off-by: skynzer <skynzer@yandex.ru>

--- bash_hw_1/hw_1.sh
+++ bash_hw_1/hw_1.sh
@@ -10,19 +10,15 @@
  cd ./$enterto
  echo "Input names of 3 .txt files"
  read t_1 t_2 t_3
- touch ${f_1}.txt ${f_2}.txt ${f_3}.txt
+ touch ${t_1}.txt ${t_2}.txt ${t_3}.txt
  echo "Input names of 2 .json files"
  read j_1 j_2
  touch ${j_1}.json ${j_2}.json
  ls -la
- i="0"
- while [ $i -lt 2 ]
- do
-    echo "input file name for move ${f_1}.txt ${f_2}.txt ${f_3}.txt ${j_1}.json ${j_2}.json"
-    read filename
-    echo "input folder name where move file $f_1, $f_2, $f_3"
-    read foldername
-    mv ./$filename ../$foldername
-    i=$[$i+1]
-done
+
+ echo "input names of two files for move ${t_1}.txt ${t_2}.txt ${t_3}.txt ${j_1}.json ${j_2}.json"
+ read filename1 filename2
+ echo "input folder name where move file $f_1, $f_2, $f_3"
+ read foldername
+ mv ./${filename1} ./${filename2} ../$foldername
  
